f76c773aaf9b3c8a5cbe2c792eb65324dce2a1ea
HrutvikKhatkar <147256332+HrutvikKhatkar@users.noreply.github.com>
2024-07-29 18:17:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ app.post('/login', async (request, response) => {
       const payload = {
         username: username,
       }
-      const jwtToken = jwt.sign(payload, 'MY_SECRET_TOKEN')
+      const jwtToken = jwt.sign(payload, 'jwt_token')
       response.send({jwtToken})
     } else {
       response.status(400)
