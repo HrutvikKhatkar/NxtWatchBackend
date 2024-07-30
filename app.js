6d40c6f6c37f2a05390210c2b094aaa7dcabd16c
HrutvikKhatkar <147256332+HrutvikKhatkar@users.noreply.github.com>
2024-07-30 09:39:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const sqlite3 = require('sqlite3');
 const { open } = require('sqlite');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const dbPath = path.join(__dirname, 'userData.db');
